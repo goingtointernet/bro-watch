5413f8731a9e383268be1b7772bca5d0f087c463
goingtointernet <goingtointernet@gmail.com>
2024-12-10 14:13:06 +0500

    Fix query and logo size

--- getsvgicons/settings.py
+++ getsvgicons/settings.py
@@ -151,10 +151,10 @@ AUTH_USER_MODEL = 'account.User'
 from decouple import config
 #SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.chengmail.cn'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True 
-EMAIL_USE_SSL = False
+EMAIL_HOST = 'smtp.126.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False 
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD =  config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
